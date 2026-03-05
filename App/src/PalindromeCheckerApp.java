e8211188756a6bde157d47f8722714820c0efec8
1346-pranav-io <pranavpooviahkh@gmail.com>
2026-03-06 01:36:27 +0530

    Pranav Added UC-1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class PalindromeCheckerApp {
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to Palindrome Checker App Management System");
+    }
+
+}
